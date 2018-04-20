2c27a479af115ff84d123d552ff9a533a6dd80ed
Unknown <kaifer741@gmail.com>
2018-04-20 21:45:48 +0200

    fgssdf

--- tester.py
+++ tester.py
@@ -2,9 +2,9 @@ from algoritmy import BasicLift, MinDistLift, MinDistCollect, RandomLift
 from multiElev import MultiElev
 from prostredi import Entv, Generator
 
-env = Entv(get_new=Generator([0.001] + ([0.0001]*99)), height=100, door_time=10, ticks=10**6)
+env = Entv(get_new=Generator([0.001] + ([0.0001]*99)), height=100, door_time=10, ticks=10**5)
 print(env.test(RandomLift()))
 print(env.test(BasicLift()))
 print(env.test(MinDistLift()))
 print(env.test(MinDistCollect()))
-print(env.test(MultiElev()))
+#print(env.test(MultiElev()))
